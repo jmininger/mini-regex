4e576cd07d4571ea5cd79708a5f7a2451730b7a7
Jacquin Mininger <jacquin.mininger@gmail.com>
2018-09-04 05:35:24 -0400

    Added tests to Stack class and allow the push() method to take variable inputs

--- stack.py
+++ stack.py
@@ -1,4 +1,5 @@
 from collections import deque
+import unittest as ut
 
 
 class Stack:
@@ -8,8 +9,30 @@ class Stack:
     def __init__(self, iterable=[]):
         self._deque = deque(iterable)
 
-    def pop(self):
-        self._deque.pop()
+    def __iter__(self):
+        while self._deque:
+            yield self.pop()
 
-    def push(self, elem):
-        self._deque.append(elem)
+    def pop(self):
+        return self._deque.pop()
+
+    def top(self):
+        return self._deque[-1]
+
+    def push(self, *args):
+        for elem in args:
+            self._deque.append(elem)
+
+
+class StackTest(ut.TestCase):
+    def test_accepts_variable_inputs(self):
+        s = Stack()
+        s.push(*[2, 4, 6])
+        actual = []
+        for elem in s:
+            actual.append(elem)
+        self.assertListEqual(actual, [6, 4, 2])
+
+
+if __name__ == '__main__':
+    ut.main()
