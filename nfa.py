1512f594f8e9a6cb2c5d88cd1985a6249d579d4c
Jacquin Mininger <jacquin.mininger@gmail.com>
2018-10-06 16:06:08 -0400

    nfa: adds multiple transition types and tests
    Adds multiple Transition classes that each take a character and
    determine whether or not an iterator will be able to advance to the next
    state based on the input character

--- nfa.py
+++ nfa.py
@@ -1,6 +1,60 @@
 import unittest as ut
 
 
+class Transition:
+    def is_available(self, char):
+        return Exception('Abstract class')
+
+
+class CharLiteralTransition(Transition):
+    def __init__(self, char):
+        self._char = char
+
+    def is_available(self, char):
+        """ Determines if a character is a member of this particular character
+            class """
+        return self._char == char
+
+
+class MetaTransition(Transition):
+    def is_available(self, char):
+        return char != '\n'
+
+
+class CaseInsensitiveLiteralTransition(CharLiteralTransition):
+    def __init__(self, char):
+        super().__init__(char)
+
+    def is_available(self, char):
+        return self._char.upper() == char.upper()
+
+
+class TransitionTest(ut.TestCase):
+    def test_char_literal(self):
+        transition = CharLiteralTransition('a')
+        self.assertTrue(transition.is_available('a'))
+        self.assertFalse(transition.is_available('c'))
+
+    def test_metachar_dot(self):
+        transition = MetaTransition()
+        self.assertTrue(transition.is_available('a'))
+        self.assertTrue(transition.is_available('b'))
+        self.assertFalse(transition.is_available('\n'))
+
+    def test_upper_lower_case_meta(self):
+        transition = CaseInsensitiveLiteralTransition('a')
+        self.assertTrue(transition.is_available('a'))
+        self.assertTrue(transition.is_available('A'))
+
+        self.assertFalse(transition.is_available('b'))
+        self.assertFalse(transition.is_available('B'))
+
+    def test_upper_lower_case_works_with_nonalpha_chars(self):
+        transition = CaseInsensitiveLiteralTransition('-')
+        self.assertTrue(transition.is_available('-'))
+        self.assertFalse(transition.is_available('a'))
+
+
 class NFA:
     def __init__(self, trans_table, start_state, end_states):
         self._table = trans_table
@@ -35,11 +89,14 @@ class NFA:
     #         raise Exception('start state is not a valid state')
     #     self.__start_state = val
 
+    def epsilon_closure(self, state):
+        return self._table.epsilons(state)
 
+@ut.skip('')
 class NFATest(ut.TestCase):
     min_trans_table = {
-            0: ([], [1]),
-            1: ([], [])
+            0: ({}, [1]),
+            1: ({}, [])
             }
 
     def test_start_state_is_valid_state(self):
@@ -56,6 +113,32 @@ class NFATest(ut.TestCase):
                 NFA, self.min_trans_table, 0, [1, 2]
                 )
 
+    def test_calculates_epsilon_closure(self):
+        trans_table = {
+                0: ({}, [1, 2]),
+                1: ({'a': 5}, [4]),
+                2: ({}, [3]),
+                3: ({}, [1]),
+                4: ({}, []),
+                5: ({}, [6]),
+                6: ({}, [1])
+                }
+        nfa = NFA(trans_table, 0, [4])
+        # Makes sure that a closure does not include itself
+        epsilon_closure0 = nfa.epsilon_closure(0)
+        self.assertSetEqual(epsilon_closure0, set([1, 2, 3, 4]))
 
+        epsilon_closure4 = nfa.epsilon_closure(4)
+        self.assertSetEqual(epsilon_closure4, set([]))
+
+        epsilon_closure5 = nfa.epsilon_closure(5)
+        self.assertSetEqual(epsilon_closure5, set([1, 2, 3, 4, 5, 6]))
+
+
+# Add a is_final_state(nodes)
+# Given a node_id and the next character, return the next state
+#   - If there is no next state, should we return "None" or the start state?
+# Need a way to store transition entries
+#   - Cannot represent transitions as chars, they need to be token classes
 if __name__ == '__main__':
     ut.main()
